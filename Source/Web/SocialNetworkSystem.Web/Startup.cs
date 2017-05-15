719928d1c9fdcd6a1948a7c3f7a3c5d71979d7b7
Hristo Avramov <CodingFor6thTime@gmail.com>
2017-05-15 16:38:58 +0300

    Created empty web and data projects

---
+++ Source/Web/SocialNetworkSystem.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SocialNetworkSystem.Web.Startup))]
+namespace SocialNetworkSystem.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
