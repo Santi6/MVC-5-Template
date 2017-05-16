d0dd99dc1e6b5a356f7e610f65086874df6e9dcd
Hristo Avramov <CodingFor6thTime@gmail.com>
2017-05-16 12:02:02 +0300

    Added Ninject

--- Source/Web/SocialNetworkSystem.Web/Global.asax.cs
+++ Source/Web/SocialNetworkSystem.Web/Global.asax.cs
@@ -1,6 +1,7 @@
 ﻿using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using SocialNetworkSystem.Web.App_Start;
 using SocialNetworkSystem.Web.Infastructure;
 
 namespace SocialNetworkSystem.Web
