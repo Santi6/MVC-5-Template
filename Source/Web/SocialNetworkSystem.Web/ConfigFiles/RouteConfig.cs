1daf1470aca93f147ac4309715d90234ea226c29
Hristo Avramov <CodingFor6thTime@gmail.com>
2017-05-19 14:01:07 +0300

    Uninstalled not needed nuget packages

--- Source/Web/SocialNetworkSystem.Web/ConfigFiles/RouteConfig.cs
+++ Source/Web/SocialNetworkSystem.Web/ConfigFiles/RouteConfig.cs
@@ -8,6 +8,16 @@ namespace SocialNetworkSystem.Web
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("Home/Index");
+
+            routes.LowercaseUrls = true;
+            routes.MapMvcAttributeRoutes();
+
+            routes.MapRoute(
+                name: "Home",
+                url: string.Empty,
+                defaults: new { controller = "Home", action = "Index" }
+            );
 
             routes.MapRoute(
                 name: "Default",
--- Source/Web/SocialNetworkSystem.Web/Controllers/HomeController.cs
+++ Source/Web/SocialNetworkSystem.Web/Controllers/HomeController.cs
@@ -4,26 +4,8 @@ namespace SocialNetworkSystem.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public HomeController()
-        {
-        }
-
         public ActionResult Index()
         {
-            return this.View();
-        }
-
-        public ActionResult About()
-        {
-            this.ViewBag.Message = "Your application description page.";
-
-            return this.View();
-        }
-
-        public ActionResult Contact()
-        {
-            this.ViewBag.Message = "Your contact page.";
-
             return this.View();
         }
     }
