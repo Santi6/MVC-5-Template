63f1c483d4ee9e4eff92509883f039933185fdf3
Hristo Avramov <CodingFor6thTime@gmail.com>
2017-06-06 15:34:05 +0300

    Deleted unnecessary Infastructure folder in web project

--- Source/Web/SocialNetworkSystem.Web/Infastructure/Mapping/IHaveCustomMappings.cs
+++
@@ -1,9 +0,0 @@
-﻿using AutoMapper;
-
-namespace SocialNetworkSystem.Web.Infastructure.Mapping
-{
-    public interface IHaveCustomMappings
-    {
-        void CreateMappings(IMapperConfigurationExpression config);
-    }
-}
--- Source/Web/SocialNetworkSystem.Web/Infastructure/Mapping/IMapFrom.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace SocialNetworkSystem.Web.Infastructure.Mapping
-{
-    public interface IMapFrom<T>
-    {
-    }
-}
